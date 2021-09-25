ec036db78b3ca8f5e6cac392b6af8561f197e446
Luis Osta <luisosta1999@gmail.com>
2021-09-25 14:30:43 -0500

    restructuring

---
+++ example/target/debug/deps/example-e35ad727b0a2a49b.d
@@ -0,0 +1,5 @@
+/home/luisosta/rust-db-generation/example/target/debug/deps/example-e35ad727b0a2a49b: src/main.rs
+
+/home/luisosta/rust-db-generation/example/target/debug/deps/example-e35ad727b0a2a49b.d: src/main.rs
+
+src/main.rs:
