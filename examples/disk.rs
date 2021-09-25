0ce9a459d658ead2927a2c15df4cb4cc7620a4e6
Luis Osta <luisosta1999@gmail.com>
2021-09-25 15:22:33 -0500

    added example back

---
+++ examples/disk.rs
@@ -0,0 +1,35 @@
+use bson::doc;
+use std::time::Instant;
+fn user_generator() -> bson::Document {
+    return doc! {
+        "_id": bson::Bson::ObjectId(bson::oid::ObjectId::new()),
+        "name": "Jane Doe"
+    };
+}
+
+fn post_generator() -> bson::Document {
+    return doc! {
+        "_id": bson::Bson::ObjectId(bson::oid::ObjectId::new()),
+        "text": "Hello World"
+    };
+}
+
+fn main() {
+    let amount = 100;
+    let now = Instant::now();
+    let collections: Vec<(String, mongoseed::EntityGenerator)> = vec![
+        (String::from("users"), user_generator),
+        (String::from("posts"), post_generator),
+        (String::from("comments"), post_generator),
+        (String::from("communities"), post_generator),
+    ];
+
+    let mongo_uri = std::env::var("MONGO_URI").unwrap_or(String::from(""));
+    println!("Mongo URI - {}", mongo_uri);
+    mongoseed::seeding::seed_data(
+        collections,
+        mongoseed::Configurations::new(amount, mongoseed::SeedMode::Disk),
+    );
+
+    println!("Total Time - {:?}", now.elapsed());
+}
