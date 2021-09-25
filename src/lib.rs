4fcf8f75f9ed655a6615bc8e11316bb8ce9f7d85
Luis Osta <luisosta1999@gmail.com>
2021-09-25 14:40:53 -0500

    moved things into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,37 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+pub type EntityGenerator = fn() -> bson::Document;
 
-pub mod generation;
-pub use generation::*;
+pub enum SeedMode {
+    Dynamic,
+    Disk,
+}
+pub struct Configurations {
+    pub amount: i32,
+    pub mode: SeedMode,
+    pub mongo_uri: Option<String>,
+}
+
+impl Configurations {
+    pub fn new(amount: i32, mode: SeedMode) -> Configurations {
+        match mode {
+            SeedMode::Dynamic => panic!("Dynamic seed mode not implemented yet"),
+            SeedMode::Disk => Configurations {
+                amount,
+                mode,
+                mongo_uri: None,
+            },
+        }
+    }
+    pub fn new_dynamic(amount: i32, mode: SeedMode, mongo_uri: String) -> Configurations {
+        match mode {
+            SeedMode::Dynamic => Configurations {
+                amount,
+                mode,
+                mongo_uri: Some(mongo_uri),
+            },
+            SeedMode::Disk => panic!("Disk seed mode not implemented yet"),
+        }
+    }
+}
