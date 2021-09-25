c70444577ca5be4e9fc2075f9bc6c25a5fcf95d8
Luis Osta <luisosta1999@gmail.com>
2021-09-25 15:10:35 -0500

    removed example

--- example/init.sh
+++
@@ -1,4 +0,0 @@
-bash ./mongo_setup.sh
-echo "sleeping for 5 seconds before seeding data"
-mongoimport --collection users --file users.json --jsonArray --uri $MONGO_URI
-mongoimport --collection posts --file posts.json --jsonArray --uri $MONGO_URI
